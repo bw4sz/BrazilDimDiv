d950ddb67a39b1562a2cafeeae16ec03169e6198
bw4sz <benweinstein2010@gmail.com>
2014-04-25 17:08:49 -0400

    build siteXspp table

---
+++ upsampletable.R
@@ -0,0 +1,65 @@
+###Create a siteXspp table at a new resolution 
+
+require(reshape)
+require(foreach)
+require(doSNOW)
+
+##read in data
+
+droppath<-"C:/Users/Jorge/Dropbox"
+
+siteXspp<-read.csv(paste(droppath,"Dimensions Data/siteXspp.csv",sep="/"),nrow=1000)
+
+#create a molleweide raster with the desired resolution
+a<-raster()
+res(a)<-1
+
+#project raster
+a.proj<-projectRaster(a,crs="+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs")
+
+#Get XY coordinates
+XY<-colsplit(siteXspp$X," ",c("X","Y"))
+
+#get the cell number from the projected raster
+cellnumber<-cellFromXY(a.proj,XY)
+
+#create an index of XY to cell number
+sp.index<-data.frame(XY=siteXspp$X,cellnumber)
+
+#get a list of unique cell numbers
+uC<-unique(cellnumber)
+
+#Combine columns with same cells
+#walk through each cell
+cl<-makeCluster(4,"SOCK")
+registerDoSNOW(cl)
+newsiteXspp<-foreach(x=1:length(uC),.packages="raster") %dopar% {
+  target<-uC[x]
+  
+  #get all the XY coords that fall in that cell
+  targetXY<-sp.index[sp.index$cellnumber %in% target,"XY"]
+  
+  #get all the rows in the Xy targets
+  tocombine<-siteXspp[siteXspp$X %in% targetXY,]
+  
+  #merge target rows
+  combrow<-(apply(tocombine[,-1],2,sum) > 0)*1
+  
+  #get the centroid of the new cell
+  newC<-xyFromCell(a.proj,target)
+  out<-data.frame(cbind(XY=newC,t(as.matrix(combrow))))
+  return(out)
+}
+stopCluster(cl)
+
+#Bind together
+newsiteXsppdf<-rbind.fill(newsiteXspp)
+
+#visualize richness to check accuracy
+cellnumberR<-cellFromXY(a.proj,cbind(newsiteXsppdf$x,y=newsiteXsppdf$y))
+
+richness<-apply(newsiteXsppdf,1,sum)
+
+a.proj[cellnumberR]<-richness
+
+plot(a.proj)
