b0aecac061c9281bd5b3c8c88b35aeb4fc879ed7
bw4sz <benweinstein2010@gmail.com>
2014-05-05 17:24:14 -0400

    combinetable needs to be fixed

--- cluster/combinetable.R
+++ cluster/combinetable.R
@@ -6,12 +6,22 @@ droppath<-"/home1/02443/bw4sz/GlobalMammals/"
 
 setwd(droppath)
 
-xytab<-fread("xytable.csv")[,-1,with=F]
+xytab<-fread("xytable.csv",nrows=10)[,-1,with=F]
 
-setnames(xytab,colnames(xytab),c("V1","x","y","id"))
+setnames(xytab,colnames(xytab),c("V1","x","y","id2"))
 
 xytab[,id:=as.character(xytab$id)]
 setkey(xytab,id)
+
+# create all combinations of 2
+# return as a data.table with these as columns `V1` and `V2`
+# then count the numbers in each group
+xytab[, rbindlist(combn(V1,2, 
+                      FUN = function(x) as.data.table(as.list(x)), simplify = F))]
+
+
+comb.table<-data.table(xytab[V1 %in% combs[,1]],xytab[V1 %in% combs[,2]])
+setnames(comb.table,colnames(comb.table),c("To.Original",'To.x',"To.y","To.id","From.Original",'From.x',"From.y","From.id"))
 
 beta_out<-fread("beta_out.csv")
 
@@ -21,7 +31,7 @@ print(head(beta_out))
 setkey(beta_out,To)
 
 #merge with original xytab
-head(Tomerge<-xytab[beta_out,])
+head(Tomerge<-beta_out[xytab])
 
 #rename the columns
 setnames(Tomerge,colnames(Tomerge),c("To.id","To.OriginalRow",'To.x',"To.y","From.id","BetaSim","Sorenson","MNTD"))
