a627e1803d0cbd76436ebcaa664c36162afd3a9b
caterina penone <caterina.penone@gmail.com>
2014-09-08 19:10:45 -0400

    minor changes in combinetables to solve a mismatch problem between coordinates and betas

--- cluster/combinetables.R
+++ cluster/combinetables.R
@@ -3,142 +3,145 @@ require(data.table)
 #setwd
 droppath<-"/work/02443/bw4sz/GlobalMammals/"
 
-# #Betadiversity data
-# dat<-fread("Output/FinalData.txt")
+# #Betadiversity data ##Cp from here to Line 146 it was deleted so I removed all the "#"
+dat<-fread("Output/FinalData.txt")
 
-# print("Beta")
-# print(dat)
+print("Beta")
+print(dat)
 
-# ###Step 1## Match unique ID to rowID and xytable
+###Step 1## Match unique ID to rowID and xytable
 # ############################################################################
-# xytable<-fread("Output/xytable.csv")[,-1,with=F]
+xytable<-fread("Output/xytable.csv")[,-1,with=F]
 
-# print(xytable)
+print(xytable)
 
-# # create all combinations of 2
-# # return as a data.table with these as columns `V1` and `V2`
-# # then count the numbers in each group
+# create all combinations of 2
+# return as a data.table with these as columns `V1` and `V2`
+# then count the numbers in each group
 
-# system.time(combs<-as.data.table(t(combn(xytable$V1,2, simplify = T))))
+system.time(combs<-as.data.table(t(combn(xytable$V1,2, simplify = T))))
 
-# print(combs)
+print(combs)
 
-# print(gc())
+print(gc())
 
-# #Merge xy info, set keys
-# setkey(combs,V1)
-# setkey(xytable,V1)
+#Merge xy info, set keys
+setkey(combs,V1)
+setkey(xytable,V1)
 
-# #combine both ways
-# combsV1<-xytable[combs]
-# setkey(combsV1,V2)
-# combsV2<-xytable[combsV1]
+#combine both ways
+combsV1<-xytable[combs]
+setkey(combsV1,V2)
+combsV2<-xytable[combsV1]
 
-# #set names
-# setnames(combsV2,colnames(combsV2),c("To.OriginalRow","To.xcord","To.ycord","To","From.OriginalRow","From.xcord","From.ycord","From"))
+#set names
+setnames(combsV2,colnames(combsV2),c("From.OriginalRow","From.xcord","From.ycord","From","To.OriginalRow","To.xcord","To.ycord","To")) #CP inverted to and from because to is 1:n and from 1:n+1
+#setnames(combsV2,colnames(combsV2),c("To.OriginalRow","To.xcord","To.ycord","To","From.OriginalRow","From.xcord","From.ycord","From"))
 
-# print("xytable")
-# print(combsV2)
-# rm(combs)
+print("xytable")
+print(combsV2)
+rm(combs)
 
-# #########################################################################
-# #Merge with betadiversity calculations
+#########################################################################
+#Merge with betadiversity calculations
 
-# #Bring in pairwise betadiveristy data
-# print(head(dat))
+#Bring in pairwise betadiveristy data
+print(head(dat))
 
-# #set keys to match
-# setkey(dat,To,From)
+#set keys to match
+setkey(dat,To,From)
 
-# #set to character, just help makes everyone match
-# combsV2[,To:=as.character(To)]
-# combsV2[,From:=as.character(From)]
+#set to character, just help makes everyone match
+combsV2[,To:=as.character(To)]
+combsV2[,From:=as.character(From)]
 
-# dat[,To:=as.character(To)]
-# dat[,From:=as.character(From)]
+dat[,To:=as.character(To)]
+dat[,From:=as.character(From)]
 
-# #now set key
-# setkey(combsV2,To,From)
+#now set key
+setkey(combsV2,To,From)
 
-# #first merge
-# mergeL<-merge(combsV2,dat)
+#first merge
+datxy<-merge(combsV2,dat) #CP name changed because I deleted the second merge (see below)
 
-# print("mergeL")
+print("datxy") #CP idem
 
-# print(mergeL)
+print(datxy) #CP idem
 
-# #Switch to and from column name
-# setnames(combsV2,colnames(combsV2),c("To.OriginalRow","To.xcord","To.ycord","From","From.OriginalRow","From.xcord","From.ycord","To"))
+#Switch to and from column name 
+#CP if you change to/from you should also change the coordinates and original rows
+#CP and in that case this row is not useful - so I deleted this part
+#setnames(combsV2,colnames(combsV2),c("To.OriginalRow","To.xcord","To.ycord","From","From.OriginalRow","From.xcord","From.ycord","To")) 
 
-# #merge again
-# mergeR<-merge(combsV2,dat)
+#merge again
+#mergeR<-merge(combsV2,dat) #CP deleted (see previous rows)
 
-# print("mergeR")
-# print(mergeR)
+#print("mergeR") #CP
+#print(mergeR) #CP
 
-# print(gc())
+print(gc())
 
-# rm(dat)
-# rm(combsV2)
-# rm(combsV1)
-# rm(xytable)
+rm(dat)
+rm(combsV2)
+rm(combsV1)
+rm(xytable)
 
-# tables()
+tables()
 
-# print(gc())
+print(gc())
 
-# datxy<-rbindlist(list(mergeR,mergeL))
+#datxy<-rbindlist(list(mergeR,mergeL)) #CP
 
-# print("datxy")
-# print(datxy)
+print("datxy")
+print(datxy)
 
-# #remove unwanted columns
-# datxy[,To:=NULL]
-# datxy[,From:=NULL]
+#remove unwanted columns
+datxy[,To:=NULL]
+datxy[,From:=NULL]
 
-# rm(mergeR)
-# rm(mergeL)
-# ls()
+#rm(mergeR) #CP
+rm(mergeL)
+ls()
 
-# ######################################################################
-# #Merge with xydistance
-# #xydist dist table
+######################################################################
+#Merge with xydistance
+#xydist dist table
 
-# xydist<-fread("Output/xydist.txt")[,-1,with=F]
+xydist<-fread("Output/xydist.txt")[,-1,with=F]
 
-# setnames(xydist,colnames(xydist),c("To.OriginalRow","From.OriginalRow","km"))
+setnames(xydist,colnames(xydist),c("To.OriginalRow","From.OriginalRow","km"))
 
-# print(xydist)
+print(xydist)
 
-# setkey(datxy,To.OriginalRow,From.OriginalRow)
-# setkey(xydist,To.OriginalRow,From.OriginalRow)
+setkey(datxy,To.OriginalRow,From.OriginalRow)
+setkey(xydist,To.OriginalRow,From.OriginalRow)
 
-# a<-merge(xydist,datxy)
+a<-merge(xydist,datxy)
 
-# print(a)
+print(a)
 
-# ## reverse names
-# setnames(xydist,colnames(xydist),c("From.OriginalRow","To.OriginalRow","km"))
+#reverse names
+setnames(xydist,colnames(xydist),c("From.OriginalRow","To.OriginalRow","km"))
 
-# # set keys
-# setkey(xydist,To.OriginalRow,From.OriginalRow)
+#set keys
+setkey(xydist,To.OriginalRow,From.OriginalRow)
 
-# b<-merge(xydist,datxy)
+b<-merge(xydist,datxy)
 
-# d<-rbindlist(list(a,b))
+d<-rbindlist(list(a,b))
 
-# rm(a)
-# rm(b)
-# rm(datxy)
-# rm(xydist)
+rm(a)
+rm(b)
+rm(datxy)
+rm(xydist)
 
-# print(gc())
+print(gc())
 
-# d<-unique(d)
+d<-unique(d)
 
-# print(d)
+print(d)
 
-# save.image("Output/BetaImage.RData")
+save.image("Output/BetaImage.RData")
 
 ############################################################################
 #Merge with environmental betadiversity
