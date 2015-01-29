e5cc48f62a73041afd4a99732f6c920c0424a711
bw4sz <benweinstein2010@gmail.com>
2015-01-29 11:57:38 -0500

    Beginning null model

--- cluster/uniqueRich_null.R
+++ cluster/uniqueRich_null.R
@@ -5,8 +5,8 @@ suppressMessages(require(data.table,quietly=TRUE,warn.conflicts=FALSE))
 suppressMessages(require(picante,quietly=TRUE,warn.conflicts=FALSE))
 
 ##set path
-droppath<-"/work/02443/bw4sz/GlobalMammals/"
-#droppath<-"C:/Users/Caterina/Dropbox/R/1312_beta"
+#droppath<-"/work/02443/bw4sz/GlobalMammals/"
+droppath<-"C:/Users/Ben/Documents/BrazilDimDiv/cluster/"
 setwd(droppath)
 
 ########################################
@@ -45,4 +45,60 @@ splist<-splist[splist %in% c(tree$tip.label)]
 
 rm(siteXspp)
 
-write.csv(dt.unique,"Input/UniquesiteXrich.csv",row.names=FALSE)
+##We need a null model from every richness type to every other richness type. Including to itself.
+ident<-expand.grid(dt.unique$rich,dt.unique$rich)
+dim(ident)
+#should be roughly
+200* 200
+
+#looks good.
+
+#Okay so now build a function that randomly draws that many species for each community and computes the metric
+x<-1
+
+sampleS<-function(x,ident){
+  row<-ident[x,]
+  #Get species for assemblage A
+  A<-sample(splist,size=row[[1]],replace=F)
+  # Get species for Assemblage B
+  B<-sample(splist,size=row[[2]],replace=F)
+  
+  #Format the data frame
+}
+
+library(pbdMPI,quietly=TRUE,warn.conflicts=FALSE,verbose=FALSE)
+
+init()
+
+#set position within the loop
+
+
+#library libraries
+suppressMessages(library(reshape2,quietly=TRUE,warn.conflicts=FALSE))
+suppressMessages(library(picante,quietly=TRUE,warn.conflicts=FALSE))
+suppressMessages(library(ggplot2,quietly=TRUE,warn.conflicts=FALSE))
+suppressMessages(library(parallel,quietly=TRUE,warn.conflicts=FALSE))
+suppressMessages(library(foreach,quietly=TRUE,warn.conflicts=FALSE))
+suppressMessages(library(GGally,quietly=TRUE,warn.conflicts=FALSE))
+suppressMessages(library(data.table,quietly=TRUE,warn.conflicts=FALSE))
+suppressMessages(library(betapart,quietly=TRUE,warn.conflicts=FALSE))
+suppressMessages(library(plyr,quietly=TRUE,warn.conflicts=FALSE))
+
+#Everyone say hello
+comm.print(comm.rank(), all.rank = TRUE)
+
+##If running locally set droppath
+
+droppath<-"/work/02443/bw4sz/GlobalMammals/"
+
+setwd(droppath)
+
+###Define Source Functions
+suppressMessages(source("Input/BrazilSourceFunctions.R"))
+
+comm.print(paste("Total nodes is:", comm.size()))
+
+############ Read in Data
+
+#set rank
+.rank<-comm.rank()+(1+position_start)
